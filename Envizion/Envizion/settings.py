2e5c22592effdbfd42c152a1e04430361bdcca40
harisch1 <haris.choudhary@outlook.com>
2021-04-15 17:09:45 +0800

    registration done

--- Envizion/Envizion/settings.py
+++ Envizion/Envizion/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Login.apps.LoginConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [

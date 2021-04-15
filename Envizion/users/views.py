cede6dc422ea84aeb6802fc0fa8d5c07d10aaf2d
harisch1 <haris.choudhary@outlook.com>
2021-04-15 20:28:26 +0800

    Updated Registration

--- Envizion/users/views.py
+++ Envizion/users/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import SignUpForm
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, 'Account Created!')
             return redirect('user-home')
     else:
-        form  = UserCreationForm()
+        form  = SignUpForm()
     return render(request,'users/register.html', {'form': form})
 
 def home(request):

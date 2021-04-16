3ff6c342a7975cd7c0b339828419abf1811ee8c7
hans2001 <hans20010907@gmail.com>
2021-04-16 15:16:57 +0800

    nice

--- Envizion/Envizion/settings.py
+++ Envizion/Envizion/settings.py
@@ -127,5 +127,3 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-LOGIN_REDIRECT_URL = 'users-home'

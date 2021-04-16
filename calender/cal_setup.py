302e67470781caf839648c9420b3fe287f7fcf06
usercspa <>
2021-04-16 04:43:32 +0000

    calendar (incomplete)

---
+++ calender/cal_setup.py
@@ -0,0 +1,39 @@
+from __future__ import print_function
+import datetime
+import os.path
+import pickle
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from calendar_api.calendar_api import google_calendar_api
+
+gCalendar=google_calendar_api()
+gCalendar.create_event(calendar_id='<your calendar id>',
+
+
+CREDENTIALS_FILE = 'credentials.json'
+
+def get_calendar_service():
+   creds = None
+   # The file token.pickle stores the user's access and refresh tokens, and is
+   # created automatically when the authorization flow completes for the first
+   # time.
+   if os.path.exists('token.pickle'):
+       with open('token.pickle', 'rb') as token:
+           creds = pickle.load(token)
+   # If there are no (valid) credentials available, let the user log in.
+   if not creds or not creds.valid:
+       if creds and creds.expired and creds.refresh_token:
+           creds.refresh(Request())
+       else:
+           flow = InstalledAppFlow.from_client_secrets_file(
+               CREDENTIALS_FILE, SCOPES)
+           creds = flow.run_local_server(port=0)
+
+       # Save the credentials for the next run
+       with open('token.pickle', 'wb') as token:
+           pickle.dump(creds, token)
+
+   service = build('calendar', 'v3', credentials=creds)
+   return service

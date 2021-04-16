fee2cd8f43ef1e5c280c51a14876d757b471f05a
harisch1 <haris.choudhary@outlook.com>
2021-04-16 21:46:54 +0800

    try

--- Envizion/blog/views.py
+++ Envizion/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.views.generic import ListView
 
 def home(request):
     context = {
@@ -8,6 +9,9 @@ def home(request):
     }
     return render(request, 'blog/home.html', context)
 
-
-
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-postDate']
 

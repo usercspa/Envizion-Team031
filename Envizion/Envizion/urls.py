ab3bd473387dfea560bff964ec2e13afe8290112
hans2001 <hans20010907@gmail.com>
2021-04-16 15:38:54 +0800

    nice

--- Envizion/Envizion/urls.py
+++ Envizion/Envizion/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as user_views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),

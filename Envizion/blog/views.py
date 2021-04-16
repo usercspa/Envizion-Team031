c2a7081e8c1be6b8f9b91f30ca5b07809adc6b35
harisch1 <haris.choudhary@outlook.com>
2021-04-16 18:39:04 +0800

    Blogs

--- Envizion/blog/views.py
+++ Envizion/blog/views.py
@@ -1,17 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
-posts = [
-    {
-        'author': 'haris',
-        'title' : 'post1',
-        'content': 'Hello, this is my first post',
-        'postDate': '21-03-2019'
-    }
-]
+
 def home(request):
     context = {
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
